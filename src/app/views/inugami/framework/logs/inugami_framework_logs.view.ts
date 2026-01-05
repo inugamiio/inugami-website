f667a6284cb34e36d3c18d2a1f17e0fd3866a8ca
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-05 05:11:17 +0100

    #428 FEAT : add documentation

---
+++ src/app/views/inugami/framework/logs/inugami_framework_logs.view.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FrameworkAsideComponent } from '../framework-aside/framework-aside.component';
+
+@Component({
+    templateUrl: './inugami_framework_logs.view.html',
+    styleUrls: ['./inugami_framework_logs.view.scss'],
+    imports:[
+            FrameworkAsideComponent
+    ]
+})
+export class InugamiFrameworkLogsView{
+    
+
+}
