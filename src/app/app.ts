a1c458edc93f18509494885e5451299b0976ffff
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-10 17:56:39 +0100

    #428 FEAT : add monitoring documentation

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,7 @@ import { ViewportScroller } from '@angular/common';
 import { GaActionEnum, GoogleAnalyticsService } from 'ngx-google-analytics';
 import { filter } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class App implements OnInit{
   private readonly gaService = inject(GoogleAnalyticsService);
+  private readonly titleService = inject(Title)
   private router = inject(Router);
   private scroller = inject(ViewportScroller);
   protected readonly title = signal('inugami-website');
@@ -91,8 +93,12 @@ export class App implements OnInit{
   }
   pageTracker(event: NavigationEnd) {
     if(event.url){
+      const urlParts = event.url.split('/');
+      const page = urlParts[urlParts.length-1];
+      this.titleService.setTitle(`Inugami - ${page.split('#')[0]}`);
       this.gaService.event(GaActionEnum.VIEW_ITEM, event.url);
       this.gaService.pageView(event.url);
+
     }
   }
 
