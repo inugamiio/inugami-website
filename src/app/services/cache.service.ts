5b1f8aac2d48050a9d6368ae35e815898515356c
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-15 23:11:19 +0100

    #428 FEAT : optimize cache

--- src/app/services/cache.service.ts
+++ src/app/services/cache.service.ts
@@ -1,30 +1,31 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+const CACHE :any = {};
+const PENDING_REQUESTS: Map<string, Observable<any>> = new Map();
 
 @Injectable({ providedIn: 'root' })
 export class CacheServices {
 
     //==================================================================================================================
-    // ATTRIBUTES
-    //==================================================================================================================
-    private cache: any = {};
-
-    //==================================================================================================================
     // API
     //==================================================================================================================
-    public set(key: string, object: any): void {
-        this.cache[key] = object;
-    }
-    public clear(key: string): void {
-        const value = this.cache[key];
-        if(value){
-            delete this.cache[key];
-        }
-    }
+  public get<T>(key: string): T | undefined {
+    return CACHE[key];
+  }
 
-    public get<T>(key: string): T | undefined {
-        const value = this.cache[key];
-        return value ? value as T : undefined;
-    }
+  public set(key: string, object: any): void {
+    CACHE[key] = object;
+    PENDING_REQUESTS.delete(key);
+  }
+
+
+  public getPending(key: string): Observable<any> | undefined {
+    return PENDING_REQUESTS.get(key);
+  }
+
+  public setPending(key: string, obs: Observable<any>): void {
+    PENDING_REQUESTS.set(key, obs);
+  }
 
 
 }
--- src/app/views/home/home.view.ts
+++ src/app/views/home/home.view.ts
@@ -1,143 +1,146 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { InuAnnoncement } from '../../components/annoncement/annoncement.component';
-import { HttpServices } from '../../services/http.service';
-import { Annoncement, AnnoncementExample, AnnoncementItem } from '../../components/annoncement/annoncement.model';
-import { InuCodeComponent } from '../../components/code/code.component';
-import { InuIcon } from 'inugami-icons';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {InuAnnoncement} from '../../components/annoncement/annoncement.component';
+import {HttpServices} from '../../services/http.service';
+import {Annoncement, AnnoncementExample, AnnoncementItem} from '../../components/annoncement/annoncement.model';
+import {InuCodeComponent} from '../../components/code/code.component';
+import {InuIcon} from 'inugami-icons';
+import {InuCite} from 'inugami-ng/components/inu-cite';
 
 
 @Component({
-    templateUrl: './home.view.html',
-    styleUrls: ['./home.view.scss'],
-    imports: [
-        InuAnnoncement,
-        InuCodeComponent,
-        InuIcon
-    ]
+  templateUrl: './home.view.html',
+  styleUrls: ['./home.view.scss'],
+  imports: [
+    InuAnnoncement,
+    InuCodeComponent,
+    InuIcon,
+    InuCite
+  ]
 })
 export class HomeView implements OnInit {
 
-    //==================================================================================================================
-    // ATTRIBUTES
-    //==================================================================================================================
-    private readonly httpServices = inject(HttpServices);
+  //==================================================================================================================
+  // ATTRIBUTES
+  //==================================================================================================================
+  private readonly httpServices = inject(HttpServices);
 
-    protected annoncements = signal<Annoncement[]>([]);
+  protected annoncements = signal<Annoncement[]>([]);
 
 
-    ngOnInit(): void {
-        this.httpServices.getSourceCode('data/annoncements.xml')
-            .subscribe({
-                next: res => {
-                    this.annoncements.set(this.parseAnnoncements(res));
-                }
-            });
+  ngOnInit(): void {
+    this.httpServices.getSourceCode('data/annoncements.xml')
+      .subscribe({
+        next: res => {
+          this.annoncements.set(this.parseAnnoncements(res));
+        }
+      });
 
+  }
+
+
+  //==================================================================================================================
+  // parsing
+  //==================================================================================================================
+  parseAnnoncements(values: string): Annoncement[] {
+    const result: Annoncement[] = [];
+    const parser = new DOMParser();
+    const node = parser.parseFromString(values, "text/xml");
+    const annoncements = node.getElementsByTagName("annoncements")[0];
+
+    for (let i = 0; i < annoncements.childNodes.length; i++) {
+      if (annoncements.childNodes[i].nodeName == 'annoncement') {
+        result.push(this.parseAnnoncement(annoncements.childNodes[i] as Element, i));
+      }
+    }
+
+    return result;
+  }
+
+  private parseAnnoncement(node: Element, index: number): Annoncement {
+    let features: AnnoncementItem[] = [];
+    let fix: AnnoncementItem[] = [];
+    let description = null;
+    for (let i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].nodeName == 'features') {
+        for (let j = 0; j < node.childNodes[i].childNodes.length; j++) {
+          if (node.childNodes[i].childNodes[j].nodeName == 'item') {
+            features.push(this.parseAnnoncementItem(node.childNodes[i].childNodes[j] as Element));
+          }
+        }
+      }
+      if (node.childNodes[i].nodeName == 'fix') {
+        for (let j = 0; j < node.childNodes[i].childNodes.length; j++) {
+          if (node.childNodes[i].childNodes[j].nodeName == 'item') {
+            fix.push(this.parseAnnoncementItem(node.childNodes[i].childNodes[j] as Element));
+          }
+        }
+      }
+      if (node.childNodes[i].nodeName == 'description') {
+        description = this.getNodeText(node.childNodes[i] as Element);
+      }
+    }
+
+    return {
+      id: index,
+      application: node.getAttribute('application'),
+      version: node.getAttribute('version'),
+      date: node.getAttribute('date'),
+      features: features,
+      fix: fix,
+      description: description
+    };
+  }
+
+  private parseAnnoncementItem(node: Element): AnnoncementItem {
+    let examples: AnnoncementExample[] = [];
+    for (let i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].nodeName == 'example') {
+        examples.push(this.parseExample(node.childNodes[i] as Element));
+      }
     }
 
 
-    //==================================================================================================================
-    // parsing
-    //==================================================================================================================
-    parseAnnoncements(values: string): Annoncement[] {
-        const result: Annoncement[] = [];
-        const parser = new DOMParser();
-        const node = parser.parseFromString(values, "text/xml");
-        const annoncements = node.getElementsByTagName("annoncements")[0];
+    return {
+      link: node.getAttribute('link'),
+      title: node.getAttribute('title'),
+      examples: examples
+    };
+  }
 
-        for (let i = 0; i < annoncements.childNodes.length; i++) {
-            if (annoncements.childNodes[i].nodeName == 'annoncement') {
-                result.push(this.parseAnnoncement(annoncements.childNodes[i] as Element, i));
-            }
-        }
+  private parseExample(node: Element): AnnoncementExample {
+    let value = null;
+    let description = null;
 
-        return result;
-    }
-    private parseAnnoncement(node: Element, index: number): Annoncement {
-        let features: AnnoncementItem[] = [];
-        let fix: AnnoncementItem[] = [];
-        let description = null;
-        for (let i = 0; i < node.childNodes.length; i++) {
-            if (node.childNodes[i].nodeName == 'features') {
-                for (let j = 0; j < node.childNodes[i].childNodes.length; j++) {
-                    if (node.childNodes[i].childNodes[j].nodeName == 'item') {
-                        features.push(this.parseAnnoncementItem(node.childNodes[i].childNodes[j] as Element));
-                    }
-                }
-            }
-            if (node.childNodes[i].nodeName == 'fix') {
-                for (let j = 0; j < node.childNodes[i].childNodes.length; j++) {
-                    if (node.childNodes[i].childNodes[j].nodeName == 'item') {
-                        fix.push(this.parseAnnoncementItem(node.childNodes[i].childNodes[j] as Element));
-                    }
-                }
-            }
-            if (node.childNodes[i].nodeName == 'description') {
-                description = this.getNodeText(node.childNodes[i] as Element);
-            }
-        }
-
-        return {
-            id: index,
-            application: node.getAttribute('application'),
-            version: node.getAttribute('version'),
-            date: node.getAttribute('date'),
-            features: features,
-            fix: fix,
-            description: description
-        };
+    for (let i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].nodeName == 'description') {
+        description = this.getNodeText(node.childNodes[i] as Element);
+      }
+      if (node.childNodes[i].nodeName == 'value') {
+        value = this.getNodeText(node.childNodes[i] as Element);
+      }
     }
 
-    private parseAnnoncementItem(node: Element): AnnoncementItem {
-        let examples: AnnoncementExample[] = [];
-        for (let i = 0; i < node.childNodes.length; i++) {
-            if (node.childNodes[i].nodeName == 'example') {
-                examples.push(this.parseExample(node.childNodes[i] as Element));
-            }
-        }
-
-
-        return {
-            link: node.getAttribute('link'),
-            title: node.getAttribute('title'),
-            examples: examples
-        };
+    return {
+      value: value == null ? '' : value,
+      description: description,
+      type: node.getAttribute('type')
     }
+  }
 
-    private parseExample(node: Element): AnnoncementExample {
-        let value = null;
-        let description = null;
+  getNodeText(node: Element): string {
+    const result: string[] = [];
 
-        for (let i = 0; i < node.childNodes.length; i++) {
-            if (node.childNodes[i].nodeName == 'description') {
-                description = this.getNodeText(node.childNodes[i] as Element);
-            }
-            if (node.childNodes[i].nodeName == 'value') {
-                value = this.getNodeText(node.childNodes[i] as Element);
-            }
-        }
+    for (let i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].nodeName == '#text') {
+        result.push((node.childNodes[i] as Text).data.replaceAll('\n', ' '));
+      }
+      if (node.childNodes[i].nodeName == '#cdata-section') {
+        result.push((node.childNodes[i] as CDATASection).data);
+      }
 
-        return {
-            value: value == null ? '' : value,
-            description: description,
-            type: node.getAttribute('type')
-        }
+
     }
-
-    getNodeText(node: Element): string {
-        const result: string[] = [];
-
-        for (let i = 0; i < node.childNodes.length; i++) {
-            if (node.childNodes[i].nodeName == '#text') {
-                result.push((node.childNodes[i] as Text).data.replaceAll('\n', ' '));
-            }
-            if (node.childNodes[i].nodeName == '#cdata-section') {
-                result.push((node.childNodes[i] as CDATASection).data);
-            }
-
-
-        }
-        return result.join(" ");
-    }
+    return result.join(" ");
+  }
 
 }
