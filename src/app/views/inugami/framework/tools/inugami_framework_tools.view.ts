f347197b9ee6796b0da0190b19e5e5b2baf936aa
Patrick Guillerm <pguillerm@smartwavesa.com>
2026-01-25 14:55:36 +0100

    add documentation on inugami tools

--- src/app/views/inugami/framework/tools/inugami_framework_tools.view.ts
+++ src/app/views/inugami/framework/tools/inugami_framework_tools.view.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {FrameworkAsideComponent} from '../framework-aside/framework-aside.component';
 import {InuDocItemComponent} from '../../../../components/doc-item/doc-item.component';
 import {InuCode} from 'inugami-ng/components/inu-code';
+import {InuCite} from 'inugami-ng/components/inu-cite';
 
 @Component({
   templateUrl: './inugami_framework_tools.view.html',
@@ -9,7 +10,8 @@ import {InuCode} from 'inugami-ng/components/inu-code';
   imports: [
     FrameworkAsideComponent,
     InuDocItemComponent,
-    InuCode
+    InuCode,
+    InuCite
   ]
 })
 export class InugamiFrameworkToolsView {
